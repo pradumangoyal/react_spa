77c63b7664b50f99d336bf12d84bc8caa224e28e
pradumangoyal <goyal.praduman611@gmail.com>
2018-06-07 09:29:44 +0530

    SPA practice

---
+++ src/Stuff.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Stuff extends Component{
+    render(){
+        return(
+            <div>
+                <h2>STUFF</h2>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+
+                <ol>
+                   <li>Nulla pulivnar dam</li>
+                   <li>Nulla pulivnar dam</li>
+                   <li>Nulla pulivnar dam</li>
+                   <li>Nulla pulivnar dam</li>
+                   <li>Nulla pulivnar dam</li>
+                </ol>
+        </div>
+
+);
+}
+}
+
+export default Stuff;
